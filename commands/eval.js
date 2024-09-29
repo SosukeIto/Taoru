39c5eaa012a3dcc3ee21152943fd1190c78e0fbf
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-09-29 13:46:26 +0900

    update: 少し修正

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,5 @@
 async function _eval(message) {
     try {
-        console.log(message.content)
         const code = message.content.replace("```", "").replace("js", "").replace("```", "").slice(6).trim();
         let evaled = eval(code);
         if (evaled instanceof Promise) {
